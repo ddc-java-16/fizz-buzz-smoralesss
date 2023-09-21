fd6cb521fd2de1133aca564626c3b5d555ae2f00
sam morales <smorales40@cnm.edu>
2023-09-21 15:07:44 -0600

    Using switch expression.

--- src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
+++ src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
@@ -5,20 +5,13 @@ public class Main {
   public static void main(String[] args) {
     for (int i = 1; i <= 100; i++) {
       int remainder = i % 15;
-      String value;
-      switch (remainder) {
-        case 0:
-         value = "fizzbuzz";
-          break;
-        case 3, 6, 9, 12:
-          value = "fizz";
-          break;
-        case 5, 10:
-          value = "buzz";
-          break;
-        default:
-          value = String.valueOf(i);
-      }
+      String value = switch (remainder) {
+        case 0 -> "fizzbuzz";
+        case 3, 6, 9, 12 -> "fizz";
+        case 5, 10 -> "buzz";
+        default -> String.valueOf(i);
+
+      };
       System.out.println(value);
 
     }
