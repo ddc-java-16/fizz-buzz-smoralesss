cf7d2f22bc7bb3cff9068b6a0d4ee630a7a9103d
sam morales <smorales40@cnm.edu>
2023-09-21 14:26:12 -0600

    Initial commit.

---
+++ src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.fizzbuzz;
+
+public class Main {
+
+  public static void main(String[] args) {
+    for (int i = 1; i <= 100; i++) {
+      if (i % 3 == 0) {
+
+      }
+
+    }
+  }
+
+}
