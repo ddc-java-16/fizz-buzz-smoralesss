4e72292ce62bac14c5e4fc939f2bd8adf82d645f
sam morales <smorales40@cnm.edu>
2023-09-21 15:23:14 -0600

    Decomposed.

--- src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
+++ src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
@@ -4,17 +4,18 @@ public class Main {
 
   public static void main(String[] args) {
     for (int i = 1; i <= 100; i++) {
-      int remainder = i % 15;
-      String value = switch (remainder) {
-        case 0 -> "fizzbuzz";
-        case 3, 6, 9, 12 -> "fizz";
-        case 5, 10 -> "buzz";
-        default -> String.valueOf(i);
-
-      };
-      System.out.println(value);
-
+      System.out.println(detectFizzBuzz(i));
     }
+  }
+
+  private static String detectFizzBuzz(int i) {
+    int remainder = i % 15;
+    return switch (remainder) {
+      case 0 -> "fizzbuzz";
+      case 3, 6, 9, 12 -> "fizz";
+      case 5, 10 -> "buzz";
+      default -> String.valueOf(i);
+    };
   }
 
 }
