635a7fea4e9808abc0cf921f564ac0a5fdb09c86
sam morales <smorales40@cnm.edu>
2023-09-21 14:50:20 -0600

    Classic switch-based solution.

--- src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
+++ src/main/java/edu/cnm/deepdive/fizzbuzz/Main.java
@@ -4,9 +4,21 @@ public class Main {
 
   public static void main(String[] args) {
     for (int i = 1; i <= 100; i++) {
-      if (i % 3 == 0) {
-
+      int remainder = i % 15;
+      switch (remainder) {
+        case 0:
+          System.out.println("fizzbuzz");
+          break;
+        case 3, 6, 9, 12:
+          System.out.println("fizz");
+          break;
+        case 5, 10:
+          System.out.println("buzz");
+          break;
+        default:
+          System.out.println(i);
       }
+
 
     }
   }
