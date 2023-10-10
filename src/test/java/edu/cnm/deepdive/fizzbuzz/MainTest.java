2b1ab84bb0a38a961cf1d2c12ab6b5cf05d90106
sam morales <smorales40@cnm.edu>
2023-10-10 10:29:32 -0600

    Added methodsource to test.

--- src/test/java/edu/cnm/deepdive/fizzbuzz/MainTest.java
+++ src/test/java/edu/cnm/deepdive/fizzbuzz/MainTest.java
@@ -5,10 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import edu.cnm.deepdive.fizzbuzz.Main.FizzBuzz;
 import java.util.EnumSet;
 import java.util.Set;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class MainTest {
@@ -48,5 +51,30 @@ class MainTest {
   @ValueSource(ints = {1, 17, 34})
   void valueOf_none(int value) {
     assertEquals(EnumSet.noneOf(FizzBuzz.class), Main.FizzBuzz.valueOf(value));
+  }
+
+  @ParameterizedTest(name = "[{index}] Assert that FizzBuzz.valueOf({0}) returns {1}.")
+  @MethodSource("valueOfTestCases")
+  void valueOf(int value, Set<FizzBuzz> expected) {
+    assertEquals(expected, Main.FizzBuzz.valueOf(value));
+  }
+
+  static Stream<Arguments> valueOfTestCases() {
+    return Stream.of(
+      Arguments.of(1, EnumSet.noneOf(FizzBuzz.class)),
+        Arguments.of(17, EnumSet.noneOf(FizzBuzz.class)),
+        Arguments.of(34, EnumSet.noneOf(FizzBuzz.class)),
+        Arguments.of(18, EnumSet.of(FizzBuzz.FIZZ)),
+        Arguments.of(9, EnumSet.of(FizzBuzz.FIZZ)),
+        Arguments.of(27, EnumSet.of(FizzBuzz.FIZZ)),
+        Arguments.of(96, EnumSet.of(FizzBuzz.FIZZ)),
+        Arguments.of(25, EnumSet.of(FizzBuzz.BUZZ)),
+        Arguments.of(55, EnumSet. of(FizzBuzz.BUZZ)),
+        Arguments.of(95, EnumSet.of(FizzBuzz.BUZZ)),
+        Arguments.of(15, EnumSet.allOf(FizzBuzz.class)),
+        Arguments.of(30, EnumSet.allOf(FizzBuzz.class)),
+        Arguments.of(1_455_555_555, EnumSet.allOf(FizzBuzz.class))
+
+    );
   }
 }
